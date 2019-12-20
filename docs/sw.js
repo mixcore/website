e5afa99617998aeef52ce86d26e8aed2c4c45c43
Smilefounder <i.love.to.smile.around@gmail.com>
2019-12-20 19:18:12 +0700

    upd

--- docs/sw.js
+++ docs/sw.js
@@ -117,9 +117,9 @@ self.addEventListener("install", function (event) {
       console.log("[PWA Builder] Caching pages during install");
 
       return cache.addAll(precacheFiles).then(function () {
-        if (offlineFallbackPage === "offline.html") {
-          return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
-        }
+        // if (offlineFallbackPage === "offline.html") {
+        //   return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
+        // }
 
         return cache.add(offlineFallbackPage);
       });
